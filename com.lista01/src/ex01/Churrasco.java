ef637a13670c14468dc330e976a97908420bf3ef
andre.augusto <apereira.asp@gmail.com>
2024-03-22 18:25:55 -0300

    Alteração atividade - 01

--- com.lista01/src/ex01/Churrasco.java
+++ com.lista01/src/ex01/Churrasco.java
@@ -27,19 +27,7 @@ public class Churrasco {
 			}
 		}
 
-
-//		//Condição para zerar o consumo de pessoas vegetarianas ou menores de 3 anos;
-//		if(!pessoa.isVegetariana() || pessoa.getIdade() >= 0 & pessoa.getIdade() <=3) {
-//
-//			if(pessoa.getIdade() >= 4 && pessoa.getIdade() <= 12) {
-//				qtdCarne = 1;
-//			}else qtdCarne = 2;
-//		}
 		return qtdCarne;
-	}
-	
-	public void mostrar() {
-		System.out.println("Consumo = " + qtdCarne);
 	}
 
 }
